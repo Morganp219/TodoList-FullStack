e4249d22543c1cff95ba84b4b215fa191537a06d
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-02-25 09:56:37 -0700

    chore: create iOS Project

---
+++ iOS/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Teaching on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
