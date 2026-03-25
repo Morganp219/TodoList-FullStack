a639c2f0407a7eec05ab1db49e4171a4315a8934
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-03-25 08:15:03 -0600

    feat: added backend details and controllers for todo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm"
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "todolist",
+    synchronize: true
+  }), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
